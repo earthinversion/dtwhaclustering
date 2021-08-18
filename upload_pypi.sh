d9e073ecd2fb41c2675281bb710ecc36bdace28d
earthinversion <utpalkumar50@gmail.com>
2021-08-18 11:02:08 +0800

    update new version

--- upload_pypi.sh
+++ upload_pypi.sh
@@ -2,4 +2,5 @@
 rm -r build dtwhaclustering.egg-info dist
 python setup.py sdist bdist_wheel
 twine check dist/*
-twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
